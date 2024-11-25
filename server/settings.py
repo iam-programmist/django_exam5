90ef696c442e6667087acd426d97d83666e1b72c
alik <qurbonovalijon30@gmail.com>
2024-11-25 11:22:46 +0500

    updating 4

--- server/settings.py
+++ server/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['alik.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'alik.onrender.com']
 
 
 # Application definition
